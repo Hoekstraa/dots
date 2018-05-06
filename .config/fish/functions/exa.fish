3f80fdfb8f8adf5b0aeb4562383a16b75a34360c
Altlock <altlockg@gmail.com>
2018-05-06 19:44:03 +0200

    new file:   functions/exa.fish 	new file:   functions/fish_prompt.fish 	new file:   functions/fisher.fish 	new file:   functions/ls.fish

---
+++ .config/fish/functions/exa.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function exa --description 'alias exa=exa -l'
+	command exa -l $argv;
+end
---
+++ .config/fish/functions/ls.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function ls --description 'alias ls=ls -Glashop'
+	command ls -Glashop $argv;
+end
