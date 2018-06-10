ede6fd82a5a6ddb8e0bd8609ef0d10e0c0f7880d
Altlock <altlockg@gmail.com>
2018-06-10 21:33:17 +0200

    Remove elpa folder

--- emacs/.emacs.d/elpa/dash-20180413.30/dash-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("dash.el") (23324 12255 632368 787000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
--- emacs/.emacs.d/elpa/popup-20160709.729/popup-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("popup.el") (23324 12025 64940 756000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; popup-autoloads.el ends here
