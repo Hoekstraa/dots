8eaee2f77e3889749740b5596c925bb72ee7e380
Jan Hoekstra <altlockg@gmail.com>
2018-09-16 17:26:23 +0200

    Add default font (FuraCode)

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -196,4 +196,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "FuraCode Nerd Font Mono" :foundry "CTDB" :slant normal :weight normal :height 113 :width normal)))))
