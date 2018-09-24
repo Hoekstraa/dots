1deb87b898a3cd8961ec236ea5ded35d113e00e0
Jan Hoekstra <altlockg@gmail.com>
2018-09-24 15:56:35 +0200

    Create modeConfig.el

---
+++ emacs/.emacs.d/modeConfig.el
@@ -0,0 +1,5 @@
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.adoc\\'" . adoc-mode))
+
+(remove-hook 'adoc-mode 'font-lock-mode)
