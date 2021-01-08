4e149e0560aa3e9bc145f2d1e76117972fee7ef3
Jan Hoekstra <altlockg@gmail.com>
2021-01-08 23:26:12 +0100

    Many a improvement here and there.

--- installer.sh
+++ installer.sh
@@ -3,25 +3,25 @@
 # Makes sure script can be called from anywhere.
 BASEDIR="$(cd "$(dirname "$0")" && pwd)"
 
-ln -sfT $BASEDIR/tmux/tmux.conf $HOME/.tmux.conf
+mkdir -vp $HOME/.config/tmux
+ln -sfT $BASEDIR/tmux/tmux.conf $HOME/.config/tmux/tmux.conf
 ln -sfT $BASEDIR/bash/bashrc $HOME/.bashrc
 ln -sfT $BASEDIR/readline/inputrc $HOME/.inputrc
-
-ln -sfT $BASEDIR/vim/vimrc $HOME/.vimrc
-mkdir -vp $HOME/.vim/
-ln -sfT $BASEDIR/vim/colors $HOME/.vim/colors
+ln -sfT $BASEDIR/vim/ $HOME/.vim
 
 mkdir -vp $HOME/.gnupg/
 ln -sfT $BASEDIR/gpg/gpg-agent.conf $HOME/.gnupg/gpg-agent.conf
 
-mkdir -vp $HOME/.config
 ln -sfT $BASEDIR/nvim $HOME/.config/nvim
 ln -sfT $BASEDIR/dunst $HOME/.config/dunst
 ln -sfT $BASEDIR/dwm $HOME/.config/dwm
 ln -sfT $BASEDIR/zathura $HOME/.config/zathura
 ln -sfT $BASEDIR/conky $HOME/.config/conky
+ln -sfT $BASEDIR/git $HOME/.config/git
 
 ln -sfT $BASEDIR/scripts $HOME/scripts
 ln -sfT $BASEDIR/fonts $HOME/.fonts
 
 ln -sfT $BASEDIR/x11/xinitrc $HOME/.xinitrc
+mkdir -vp $HOME/.config/X11
+ln -sfT $BASEDIR/x11/xserverrc $HOME/.config/X11/xserverrc
