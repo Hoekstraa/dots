152e6e76d85fc4b55955d2aa8dbf14c62ed1f394
Jan Hoekstra <altlockg@gmail.com>
2021-01-19 23:42:59 +0100

    Add mime default applications file.

--- installer.sh
+++ installer.sh
@@ -8,18 +8,20 @@ ln -sfT $BASEDIR/bash/bashrc $HOME/.bashrc
 ln -sfT $BASEDIR/readline/inputrc $HOME/.inputrc
 ln -sfT $BASEDIR/vim/ $HOME/.vim
 
-mkdir -vp $HOME/.config/cmus
-ln -sfT $BASEDIR/cmus/rc $HOME/.config/cmus/rc
-
-mkdir -vp $HOME/.gnupg/
-ln -sfT $BASEDIR/gpg/gpg-agent.conf $HOME/.gnupg/gpg-agent.conf
-
+mkdir -vp $HOME/.config/
+ln -sfT $BASEDIR/mime/mimeapps.list $HOME/.config/mimeapps.list
 ln -sfT $BASEDIR/nvim $HOME/.config/nvim
 ln -sfT $BASEDIR/dunst $HOME/.config/dunst
 ln -sfT $BASEDIR/dwm $HOME/.config/dwm
 ln -sfT $BASEDIR/zathura $HOME/.config/zathura
 ln -sfT $BASEDIR/conky $HOME/.config/conky
 ln -sfT $BASEDIR/git $HOME/.config/git
+
+mkdir -vp $HOME/.config/cmus
+ln -sfT $BASEDIR/cmus/rc $HOME/.config/cmus/rc
+
+mkdir -vp $HOME/.gnupg/
+ln -sfT $BASEDIR/gpg/gpg-agent.conf $HOME/.gnupg/gpg-agent.conf
 
 ln -sfT $BASEDIR/scripts $HOME/.scripts
 ln -sfT $BASEDIR/fonts $HOME/.fonts
