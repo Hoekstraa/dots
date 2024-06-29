f8d4f1491fbae980a18e98c568e81e1849543889
Zoë Hoekstra <altlockg@gmail.com>
2024-06-29 11:04:50 +0200

    GPG/gsettings fixes

--- gnome/settings.sh
+++ gnome/settings.sh
@@ -2,7 +2,7 @@ if ! command -v gsettings &> /dev/null
 then
 	echo "gsettings not available, not setting Gnome config"
 	exit
-fi	
+fi
 
 # gsettings list-schemas
 # gsettings list-keys <schema>
@@ -12,4 +12,4 @@ fi
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 gsettings set org.gnome.desktop.interface enable-animations false
-gsettings set org.gnome.desktop.wm.preferences.button-layout "appmenu:minimize,close"
+gsettings set org.gnome.desktop.wm.preferences button-layout "appmenu:minimize,close"
