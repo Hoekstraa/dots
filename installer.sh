acf3b9b153a045b1cda45f0df8e777fba35ba76f
Zoë Hoekstra <altlockg@gmail.com>
2024-07-29 09:13:37 +0200

    Move editor function to script, use XDG where possible, add newsboat URLs

--- installer.sh
+++ installer.sh
@@ -5,26 +5,27 @@ BASEDIR="$(cd "$(dirname "$0")" && pwd)"
 PLATFORM="$(uname | tr '[:upper:]' '[:lower:]')"
 
 link() {
-	# ln -sfT replacement in case of Mac OS
-	case $PLATFORM in
-		linux*)
-			ln -sfT "$1" "$2"
-			;;
-		darwin*)
-			ln -sfh "$1" "$2"
-			;;
-		*)
-			ln -sfT "$1" "$2"
-			;;
-	esac
+    # ln -sfT replacement in case of Mac OS
+    case $PLATFORM in
+        linux*)
+            ln -sfT "$1" "$2"
+            ;;
+        darwin*)
+            ln -sfh "$1" "$2"
+            ;;
+        *)
+            ln -sfT "$1" "$2"
+            ;;
+    esac
 }
 
-link "$BASEDIR/tmux/tmux.conf" "$HOME/.tmux.conf"
 link "$BASEDIR/bash/bashrc" "$HOME/.bashrc"
-link "$BASEDIR/readline/inputrc" "$HOME/.inputrc"
+link "$BASEDIR/bash/bashrc" "$HOME/.bash_profile"
 link "$BASEDIR/vim/" "$HOME/.vim"
 
 mkdir -vp "$HOME/.config/"
+link "$BASEDIR/tmux/" "$HOME/.config/tmux"
+link "$BASEDIR/readline/" "$HOME/.config/readline"
 link "$BASEDIR/mime/mimeapps.list" "$HOME/.config/mimeapps.list"
 link "$BASEDIR/nvim" "$HOME/.config/nvim"
 link "$BASEDIR/dunst" "$HOME/.config/dunst"
@@ -35,14 +36,10 @@ link "$BASEDIR/conky" "$HOME/.config/conky"
 link "$BASEDIR/git" "$HOME/.config/git"
 link "$BASEDIR/newsboat" "$HOME/.config/newsboat"
 link "$BASEDIR/karabiner" "$HOME/.config/karabiner"
+link "$BASEDIR/cmus/" "$HOME/.config/cmus"
 
 link "$BASEDIR/ssh/config" "$HOME/.ssh/config"
 link "$BASEDIR/ssh/config.d" "$HOME/.ssh/config.d"
-
-
-
-mkdir -vp "$HOME/.config/cmus"
-link "$BASEDIR/cmus/rc" "$HOME/.config/cmus/rc"
 
 mkdir -vp "$HOME/.gnupg/"
 link "$BASEDIR/gpg/gpg-agent.conf" "$HOME/.gnupg/gpg-agent.conf"
@@ -50,20 +47,26 @@ link "$BASEDIR/gpg/gpg-agent.conf" "$HOME/.gnupg/gpg-agent.conf"
 link "$BASEDIR/scripts" "$HOME/.scripts"
 
 case $PLATFORM in
-	darwin*)
-		find "$BASEDIR/fonts/" -name "*.otf" -type f -exec cp {} ~/Library/Fonts \;
-		find "$BASEDIR/fonts/" -name "*.ttf" -type f -exec cp {} ~/Library/Fonts \;
-		;;
-	*)
-		link "$BASEDIR/fonts" "$HOME/.fonts"
-		;;
+    darwin*)
+        find "$BASEDIR/fonts/" -name "*.otf" -type f -exec cp {} ~/Library/Fonts \;
+        find "$BASEDIR/fonts/" -name "*.ttf" -type f -exec cp {} ~/Library/Fonts \;
+        ;;
+    *)
+        link "$BASEDIR/fonts" "$HOME/.fonts"
+        ;;
 esac
-
 
 link "$BASEDIR/x11/xinitrc" "$HOME/.xinitrc"
 link "$BASEDIR/x11/xserverrc" "$HOME/.xserverrc"
 
 # For 24-bit color in CLI Emacs, a terminfo definition needs to exist
-tic -x -o ~/.terminfo "$BASEDIR/terminfo/xterm-24bit.terminfo"
+case $PLATFORM in
+    darwin*)
+        ;;
+    *)
+        tic -x -o ~/.terminfo "$BASEDIR/terminfo/xterm-24bits.terminfo"
+        ;;
+esac
+
 ./gnome/settings.sh
 ./pkgs/install-pkgs.sh
