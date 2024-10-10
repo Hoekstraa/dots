746fd10a67fbecd13aac257ac4493f42f145850f
Zoë Hoekstra <altlockg@gmail.com>
2024-10-10 16:48:31 +0200

    Small changes to git/ssh/gpg setup

--- installer.sh
+++ installer.sh
@@ -5,22 +5,23 @@ BASEDIR="$(cd "$(dirname "$0")" && pwd)"
 PLATFORM="$(uname | tr '[:upper:]' '[:lower:]')"
 
 link() {
-    # ln -sfT replacement in case of Mac OS
-    case $PLATFORM in
-        linux*)
-            ln -sfT "$1" "$2"
-            ;;
-        darwin*)
-            ln -sfh "$1" "$2"
-            ;;
-        *)
-            ln -sfT "$1" "$2"
-            ;;
-    esac
+		# ln -sfT replacement in case of Mac OS
+		case $PLATFORM in
+				linux*)
+						ln -sfT "$1" "$2"
+						;;
+				darwin*)
+						ln -sfh "$1" "$2"
+						;;
+				*)
+						ln -sfT "$1" "$2"
+						;;
+		esac
 }
 
 link "$BASEDIR/bash/bashrc" "$HOME/.bashrc"
 link "$BASEDIR/bash/bashrc" "$HOME/.bash_profile"
+link "$BASEDIR/zsh/zshrc" "$HOME/.zshrc"
 link "$BASEDIR/vim/" "$HOME/.vim"
 
 mkdir -vp "$HOME/.config/"
@@ -47,25 +48,28 @@ link "$BASEDIR/gpg/gpg-agent.conf" "$HOME/.gnupg/gpg-agent.conf"
 link "$BASEDIR/scripts" "$HOME/.scripts"
 
 case $PLATFORM in
-    darwin*)
-        find "$BASEDIR/fonts/" -name "*.otf" -type f -exec cp {} ~/Library/Fonts \;
-        find "$BASEDIR/fonts/" -name "*.ttf" -type f -exec cp {} ~/Library/Fonts \;
-        ;;
-    *)
-        link "$BASEDIR/fonts" "$HOME/.fonts"
-        ;;
+		darwin*)
+				find "$BASEDIR/fonts/" -name "*.otf" -type f -exec cp {} ~/Library/Fonts \;
+				find "$BASEDIR/fonts/" -name "*.ttf" -type f -exec cp {} ~/Library/Fonts \;
+				;;
+		*)
+				link "$BASEDIR/fonts" "$HOME/.fonts"
+				;;
 esac
 
 link "$BASEDIR/x11/xinitrc" "$HOME/.xinitrc"
 link "$BASEDIR/x11/xserverrc" "$HOME/.xserverrc"
 
-# For 24-bit color in CLI Emacs, a terminfo definition needs to exist
 case $PLATFORM in
-    darwin*)
-        ;;
-    *)
-        tic -x -o ~/.terminfo "$BASEDIR/terminfo/xterm-24bits.terminfo"
-        ;;
+		darwin*)
+
+				# Disable keychain saving in pinentry
+				defaults write org.gpgtools.common UseKeychain NO
+				;;
+		*)
+				# For 24-bit color in CLI Emacs, a terminfo definition needs to exist
+				tic -x -o ~/.terminfo "$BASEDIR/terminfo/xterm-24bits.terminfo"
+				;;
 esac
 
 ./gnome/settings.sh
